55ac83dff2e8767bc9130acae0787f183e770c05
Jose Neto <zeneto2978@yahoo.com.br>
2026-05-25 11:47:55 -0500

    Created Assignment and AssignmentRepository

--- src/main/java/com/jose/texaslogistics/assignment/Assignment.java
+++ src/main/java/com/jose/texaslogistics/assignment/Assignment.java
@@ -1,4 +1,60 @@
 package com.jose.texaslogistics.assignment;
 
+
+import com.jose.texaslogistics.driver.Driver;
+import com.jose.texaslogistics.shipment.Shipment;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "assignments")
 public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_id", nullable = false)
+    private Driver driver;
+
+    @ManyToOne
+    @JoinColumn(name = "shipment_id", nullable = false)
+    private Shipment shipment;
+
+    private LocalDateTime assignedAt;
+
+    public Assignment() {
+
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.assignedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Shipment getShipment() {
+        return shipment;
+    }
+
+    public void setShipment(Shipment shipment) {
+        this.shipment = shipment;
+    }
+
+    public LocalDateTime getAssignedAt() {
+        return assignedAt;
+    }
 }
---
+++ src/main/java/com/jose/texaslogistics/assignment/AssignmentRepository.java
@@ -0,0 +1,6 @@
+package com.jose.texaslogistics.assignment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+}
